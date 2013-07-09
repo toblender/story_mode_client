ba942ae129c5374f6b196d0958ae8bb0644cd1ed
Danny Hui <danny@defyent.com>
2013-07-09 17:00:48 -0400

    Wrote a custom filter for URL.

---
+++ app/scripts/filters/css-background-url.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('cssBackgroundURL',[]).filter('filterURL',function(){
+    return function(input){
+        input = input ? input: "";
+        return input.substring(0,3) == 'url' ? input:'url('+input+')';
+    }
+});
---
+++ app/scripts/filters/filterUrl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('URLFilter',[]).filter('URLFilter',function(){
+    return function(input){
+        return input.substring(0,2) == 'url' ? 'uhoh':input;
+    }
+});
